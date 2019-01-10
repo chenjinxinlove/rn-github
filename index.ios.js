6a2fd2f54211dcac255004414cf3c92848d34c6a
chenjinxinlove <505150704@qq.com>
2019-01-10 08:56:04 +0800

    可以正常的启动ios

--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react'
 import { AppRegistry, StyleSheet, Image, Text, View, CustomBadgeView } from 'react-native'
-import TabNavigator from 'react-native-tab-navigator'
+import { TabNavigator, TabNavigatorItem } from 'react-native-tab-navigator'
 
 export default class rngithub extends Component {
 	constructor(props) {
@@ -18,138 +18,7 @@ export default class rngithub extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<TabNavigator>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'tb_popular'}
-						seselectedTitleStyle={{color: 'red'}}
-						title="Home"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_polular.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_polular.png')}
-							/>
-						)}
-						badgeText="1"
-						onPress={() => this.setState({ selectedTab: 'home' })}
-					>
-						<View style={styles.page1} />
-					</TabNavigator.Item>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'tb_trending'}
-						seselectedTitleStyle = {
-							{
-								color: '#ffd'
-							}
-						}
-						title="Profile"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_trending.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_trending.png')}
-							/>
-						)}
-						renderBadge={() => <CustomBadgeView />}
-						onPress={() => this.setState({ selectedTab: 'tb_favorite' })}
-					>
-						<View style={styles.page2} />
-					</TabNavigator.Item>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'tb_my'}
-						seselectedTitleStyle = {{color: '#ffd'}}
-						title="Profile"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_trending.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_trending.png')}
-							/>
-						)}
-						renderBadge={() => <CustomBadgeView />}
-						onPress={() => this.setState({ selectedTab: 'profile' })}
-					>
-						<View style={styles.page2} />
-					</TabNavigator.Item>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'profile'}
-						seselectedTitleStyle = {{color: '#ffd'}}
-						title="Profile"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_trending.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_trending.png')}
-							/>
-						)}
-						renderBadge={() => <CustomBadgeView />}
-						onPress={() => this.setState({ selectedTab: 'profile' })}
-					>
-						<View style={styles.page2} />
-					</TabNavigator.Item>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'profile'}
-						seselectedTitleStyle = {{color: '#ffd'}}
-						title="Profile"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_trending.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_trending.png')}
-							/>
-						)}
-						renderBadge={() => <CustomBadgeView />}
-						onPress={() => this.setState({ selectedTab: 'profile' })}
-					>
-						<View style={styles.page2} />
-					</TabNavigator.Item>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'profile'}
-						seselectedTitleStyle={{ color: '#ffd' }}
-						title="Profile"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_trending.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_trending.png')}
-							/>
-						)}
-						renderBadge={() => <CustomBadgeView />}
-						onPress={() => this.setState({ selectedTab: 'profile' })}
-					>
-						<View style={styles.page2} />
-					</TabNavigator.Item>
-					<TabNavigator.Item
-						selected={this.state.selectedTab === 'profile'}
-						seselectedTitleStyle={{ color: '#ffd' }}
-						title="Profile"
-						renderIcon={() => (
-							<Image style={styles.image} source={require('./res/images/ic_trending.png')} />
-						)}
-						renderSelectedIcon={() => (
-							<Image
-								style={[styles.image, { tintColor: 'red' }]}
-								source={require('./res/images/ic_trending.png')}
-							/>
-						)}
-						renderBadge={() => <CustomBadgeView />}
-						onPress={() => this.setState({ selectedTab: 'profile' })}
-					>
-						<View style={styles.page2} />
-					</TabNavigator.Item>
-				</TabNavigator>
+				<Text>dfsdfsdjjj</Text>
 			</View>
 		)
 	}
